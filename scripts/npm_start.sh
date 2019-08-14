a5dab09451a906b2077099ac3534b38e86e53331
Anh <ltanh2710@gmail.com>
2019-08-15 01:05:42 +0700

    build

--- scripts/npm_start.sh
+++ scripts/npm_start.sh
@@ -2,10 +2,4 @@
 cd /var/www/html
 npm run build
 
-# cd /var/www
-# mkdir -p build
-# cp /var/www/html/build/* /var/www/build/
-# rm -rf /var/www/html/*
-# cp /var/www/build/* /var/www/html/
 
-# rm -rf /var/www/html/build
