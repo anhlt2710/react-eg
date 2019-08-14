9ea71c94de9f7893796dc5de664331a211a4f2e9
Anh <ltanh2710@gmail.com>
2019-08-15 01:38:36 +0700

    rm -rf

--- scripts/npm_start.sh
+++ scripts/npm_start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /var/www
 mkdir -p build
-cp /var/www/html/build/* /var/www/build/
-# rm -rf /var/www/html/*
-cp /var/www/build/* /var/www/html/
+cp -r /var/www/html/build/* /var/www/build/
+rm -rf /var/www/html/*
+cp -r /var/www/build/* /var/www/html/
 
-# rm -rf /var/www/html/build
+rm -rf /var/www/html/build/
